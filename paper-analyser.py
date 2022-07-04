08608dcff2b9314c745c5ae3e3bc4afd4a6e72e0
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2022-07-04 14:15:31 +0200

    Add files via upload

--- paper-analyser.py
+++ paper-analyser.py
@@ -6,15 +6,25 @@ import pandas as pd
 
 # READ FILES
 file_paths = filedialog.askopenfilenames()
-files = []
+files  = []
+journal = []
 for fp in file_paths:
     
     file = open(fp, 'rb')
-
+    
     fileReader = PyPDF2.PdfFileReader(file)
     files.append(fileReader)
 
-    print(fileReader.numPages)
+    print(f"Reading {fp.split("/")[-1]} with {fileReader.numPages} pages")
+    
+    if "non_ieee" in fp:
+        journal.append("other")
+    elif "springer" in fp:
+        journal.append("springer")
+    
+    else:
+        journal.append("ieee")
+    
     
 papers = {}
 cited = {}
@@ -22,8 +32,63 @@ authorsOf = {}
 all_authors = []
 first_authors = []
 
+def get_lastname(s):
+    s = s.split(" ")
+    if len(s) == 0:
+        return s
+    
+    lastname = ""
+    for i in s:
+        if len(i) > len(lastname):
+            lastname = i
+
+    return lastname
+
+
+def parse_springer(ref):
+    global papers
+        
+    title = re.sub("\n|,", " ",re.split('\.:', ref)[1].split(".")[0]).strip().lower()
+   
+    if title in papers.keys():
+        papers[title] += 1
+    else:
+        papers[title] = 1
+
+    # GET AUTHOR
+    authors = re.sub("\n", " ",re.split('\.:', ref)[0]).strip().split(".,")
+    
+
+    # GET LAST ELEMENT, MOST LIKELY THE YEAR
+    if len(re.findall("\(([0-9]{4})\)", ref)) > 0 :
+        year = re.findall("\(([0-9]{4})\)", ref)[-1].strip()
+    else:
+        year = None
+    return title, authors, year
+        
+
+def parse_ieee(ref):
+    global papers
+    
+    title = re.sub("\n|,", " ",re.split('“|”', ref)[1]).strip().lower()
+   
+    if title in papers.keys():
+        papers[title] += 1
+    else:
+        papers[title] = 1
+
+    # GET AUTHOR
+    authors = re.sub("\n|\.", " ",re.split('“', ref)[0]).strip().split(",")
+    
+
+    # GET LAST ELEMENT, MOST LIKELY THE YEAR
+    year = re.sub("\.", " ",re.split(' ', ref)[-1]).strip()
+    return title, authors, year
+
+
+
 # LOOP OVER FILES
-for fileReader in files:
+for fileNumber, fileReader in enumerate(files):
     
     # START GETS TRUE IF "REFERENCES" IS FOUND IN THE DOC
     start = False
@@ -38,16 +103,22 @@ for fileReader in files:
             refs = re.split("\[[0-9]+\]",text)
             if len(refs) < 3:
                 refs = re.split("[0-9]+\.",text)
+        else:
+            # IF REFERENCES IS FOUND - SLICE PAGE FOR EVERYTHING AFTER REFERENCES AND THEN PARSE 
+            text = page.extract_text().lower()
 
-        # IF REFERENCES IS FOUND - SLICE PAGE FOR EVERYTHING AFTER REFERENCES AND THEN PARSE 
-        if "\nreferences\n" in page.extract_text().lower():
+            if "\nreferences\n" in text:
+                _refheading = "\nreferences\n"
+            elif "\nr\neferences\n" in text:
+                _refheading =  "\nr\neferences\n"
+            else:
+                continue
+
             start = True
-            text = page.extract_text().lower().split("\nreferences\n")[1]
+            text = page.extract_text().lower().split(_refheading)[1]
             refs = re.split("\[[0-9]+\]",text)
-            #print(text)
             if len(refs) < 3:
                 refs = re.split("[0-9]+\.",text)
-            #print(refs)
 
 
         if not start:
@@ -55,50 +126,63 @@ for fileReader in files:
 
         # PARSE REFERENCES
         for ref in refs:
+
             if ref in ["\n", ""]:
                 continue
-            if "“" not in ref:
-                continue
+            
 
             # GET TITLE AND COUNT OCCURENCIES
-            title = re.sub("\n|,", " ",re.split('“|”', ref)[1]).strip().lower()
-            if title in papers.keys():
-                papers[title] += 1
+            if journal[fileNumber] == "ieee":
+                if "“" not in ref:
+                    continue
+                title, authors, year = parse_ieee(ref)
+            elif journal[fileNumber] == "springer":
+                if ".:" not in ref:
+                    continue
+                title, authors,year = parse_springer(ref)
             else:
-                papers[title] = 1
-            
-            # GET AUTHOR
-            authors = re.sub("\n|.?and\ ", " ",re.split('“', ref)[0]).strip().split(",")
-            
-            # GET LAST ELEMENT, MOST LIKELY THE YEAR
-            year = re.sub("\.", " ",re.split(' ', ref)[-1]).strip()
-            
+                try:
+                    title, authors, year = parse_ieee(ref)
+                except:
+                    try:
+                        title, authors,year = parse_springer(ref)
+                    except:
+                        print("No success parsing ref:")
+                        print(ref)
+                        continue
+
+        
             if "" in authors:
                 authors.remove("")
             
             # GET FIRST AUTHORS
             for a in authors:
                 a = a.lower()
-                a = a.replace("and", "").strip()
-                first_authors.append(a)
-                if a in cited.keys():
-                    cited[a].append(title)
-                else:
-                    cited[a] = [title]
-                break
+                a = a.replace("and ", " ").replace("et al.", "").replace(".", "").replace(",", "").replace("  ", " ").strip()
+                a = get_lastname(a)
+                a = "".join(re.findall("([a-z]+)", a))
+                if len(a) > 1:
+                    first_authors.append(a)
+                    if a in cited.keys():
+                        cited[a].append(title)
+                    else:
+                        cited[a] = [title]
+                    break
 
                 
             # GET FULL LIST AUTHORS
             for a in authors:
                 a = a.lower()
-                a = a.replace("and", "").replace("et al.", "").strip()
-                
-                if title in authorsOf.keys():
-                    authorsOf[title].add(a)
-                else:
-                    authorsOf[title] = set([a])
-                    
-                all_authors.append(a)
+                a = a.replace("and ", " ").replace("et al.", "").replace(".", "").replace(",", "").replace("  ", " ").strip()
+                a = get_lastname(a)
+                a = "".join(re.findall("([a-z]+)", a))
+                if len(a) > 1:
+                    if title in authorsOf.keys():
+                        authorsOf[title].add(a)
+                    else:
+                        authorsOf[title] = set([a])
+
+                    all_authors.append(a)
 
 
     print(f"References found: {len(papers.keys())-t0}")

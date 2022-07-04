5089f0e0fc68e47d37829d6f9f5b8c765984ee39
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2022-07-04 07:09:59 +0200

    Update paper-analyser.py

--- paper-analyser.py
+++ paper-analyser.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from tkinter import filedialog
 import pandas as pd
 
-
+# READ FILES
 file_paths = filedialog.askopenfilenames()
 files = []
 for fp in file_paths:
@@ -22,23 +22,26 @@ authorsOf = {}
 all_authors = []
 first_authors = []
 
-
+# LOOP OVER FILES
 for fileReader in files:
+    
+    # START GETS TRUE IF "REFERENCES" IS FOUND IN THE DOC
     start = False
     t0 = len(papers.keys())
     
 
     for page in fileReader.pages:
-
+        
+        # DIRECTLY GET EVERY REFERENCE OF THE PAGE
         if start:
             text = page.extract_text()
             refs = re.split("\[[0-9]+\]",text)
             if len(refs) < 3:
                 refs = re.split("[0-9]+\.",text)
 
-
+        # IF REFERENCES IS FOUND - SLICE PAGE FOR EVERYTHING AFTER REFERENCES AND THEN PARSE 
         if "\nreferences\n" in page.extract_text().lower():
-            start = True
+            start = Thttps://github.com/Nerolation/Related-Work-Analyserrue
             text = page.extract_text().lower().split("\nreferences\n")[1]
             refs = re.split("\[[0-9]+\]",text)
             #print(text)
@@ -50,25 +53,30 @@ for fileReader in files:
         if not start:
             continue
 
-
+        # PARSE REFERENCES
         for ref in refs:
             if ref in ["\n", ""]:
                 continue
             if "“" not in ref:
                 continue
 
+            # GET TITLE AND COUNT OCCURENCIES
             title = re.sub("\n|,", " ",re.split('“|”', ref)[1]).strip().lower()
             if title in papers.keys():
                 papers[title] += 1
             else:
                 papers[title] = 1
-
+            
+            # GET AUTHOR
             authors = re.sub("\n|.?and\ ", " ",re.split('“', ref)[0]).strip().split(",")
+            
+            # GET LAST ELEMENT, MOST LIKELY THE YEAR
             year = re.sub("\.", " ",re.split(' ', ref)[-1]).strip()
             
             if "" in authors:
                 authors.remove("")
-
+            
+            # GET FIRST AUTHORS
             for a in authors:
                 a = a.lower()
                 a = a.replace("and", "").strip()
@@ -79,6 +87,8 @@ for fileReader in files:
                     cited[a] = [title]
                 break
 
+                
+            # GET FULL LIST AUTHORS
             for a in authors:
                 a = a.lower()
                 a = a.replace("and", "").replace("et al.", "").strip()
@@ -113,6 +123,7 @@ paper_count = dict(sorted(papers.items(), key=lambda item: item[1], reverse=True
 df = pd.DataFrame.from_dict(paper_count, orient="index", columns=["citations"])
 df = df.reset_index().rename(columns={'index': 'paper'})
 
+# MAP AUTHORS
 for i, j in df.iterrows():
     if j["paper"] in authorsOf.keys():
         df.loc[i, "author"] = ",".join(authorsOf[j["paper"]])
@@ -120,4 +131,5 @@ for i, j in df.iterrows():
 col = df.pop("author")
 df.insert(0, col.name, col)
     
+# SAVE
 df.to_csv("paper_summary.csv", index=None, sep=";")        
